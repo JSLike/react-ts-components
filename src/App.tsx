8b9e8cbceb7f0b01dce9e0656b4a68c0688a78eb
紫光 <like_text@163.com>
2020-07-08 17:22:26 +0800

    button按钮初步实现

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import React,{useContext} from 'react';
-import './App.css';
-import LikeButton from "./components/LikeButton";
-import {log} from "util";
-interface IContext{
-    myName:string,
-    myAge:number
-}
-let num=1;
-if(num==1){
-    
-};
-let defaultValue:IContext={
-    myName:'Like',
-    myAge:19921
-}
-export const myContext=React.createContext(defaultValue)
-function App() {
-    const AMyContext=useContext(myContext)
+import React from 'react';
+import './App.scss';
 
-    const onContextValue=():void=>{
-        console.log('get app myContext---',myContext,AMyContext)
-    }
+import Button, {ButtonSize, ButtonType} from './components/Button/Button'
+
+function App() {
     return (
         <div className={"App"}>
-            <myContext.Provider value={{myName:'is default value properties',myAge:111}}>
-                <h1>这是app主页面</h1>
-                {/*<Home />*/}
-                <LikeButton/>
-                <button onClick={onContextValue}>App的button | 点击获取context-value</button>
-            </myContext.Provider>
+            <h1>App page</h1>
 
+            <Button btnType={ButtonType.Primary} size={ButtonSize.Large}
+            >默认button</Button>
+
+            <Button btnType={ButtonType.Link}  href={'https://www.baidu.com'} disabled
+            >button-link</Button>
+
+            <Button disabled={true}>禁用标志</Button>
+            <div>
+                boolean默认值
+            </div>
         </div>
     );
 }
